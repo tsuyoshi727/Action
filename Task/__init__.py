c3dc34ebb5cfc3f5618b0e88d762c64ff9fde0a5
test <test@gmail.com>
2020-12-09 11:31:34 +0800

    update

--- Task/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# _*_ coding:utf-8 _*_
-
-# @Time    : 2020/11/29 17:07
-# @Author  : TNanko
-# @Site    : https://tnanko.github.io
-# @File    : __init__.py.py
-# @Software: PyCharm
