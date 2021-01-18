13f7585af6f2c60793657743b9c64233cf594514
wp <wp@gmail.com>
2021-01-18 12:34:48 +0800

    Add

--- XP/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-exports.main_handler = async (event, context, callback) => {
-    for (const v of event["Message"].split("\r\n")) {
-        //解决云函数热启动问题
-        delete require.cache[require.resolve(`./${v}.js`)];
-        console.log(v);
-        require(`./${v}.js`)
-    }
-}
