7cfb36afc65729baca3db3f0c30c9b49bb4439fb
ip <ip@gmail.com>
2021-03-06 16:04:20 +0800

    Add

--- Task/youthshare3.js
+++ Task/youthshare3.js
@@ -3,9 +3,9 @@ const $ = new Env('youthshare');
 let md5 = require('md5-node');
 let nowTime;
 let wxck;
-let articles = ["https://focus.youth.cn/article/s?signature=MGDKgpQNLZkJvEd4q8zojVceE63XiNvDQkw1rny295VAlmPWzY&uid=46837595&phone_code=1160e4f96e84608384d9f87f87a0a44f&scid=36663151&time=1614997755&app_version=1.7.8&sign=72e8982853ac0dfd85a01ca9069c5381",
-"https://focus.youth.cn/article/s?signature=6K3Zgj0LVrQbJw94Vj3moOcpYWLKsoP5y3g4mxB5qW8oDnvelE&uid=47692088&phone_code=6b7e436b42d6e438d0bea4c97a61c168&scid=36659895&time=1614996882&app_version=1.7.8&sign=ff6b8af905b0f7beb4c5b9b59fbc25c9",
-"https://focus.youth.cn/article/s?signature=k5Bv92bmMjwJEOP782bwpyS5mv0nhzQpReJ1gxne6rYKdpWVoR&uid=50565700&phone_code=eb56c2d93692c0871487e692b21a24d9&scid=36641888&time=1614996860&app_version=1.7.8&sign=e61d2f3897dd134b7990b2bf69025743"]
+let articles = ["https://focus.youth.cn/article/s?signature=8MzJgNdEKAO0xvq7nRGB9Jf2P5pei50xBb87ZPYQ3lm9pbD2yn&uid=46837595&phone_code=1160e4f96e84608384d9f87f87a0a44f&scid=36650442&time=1615017285&app_version=1.7.8&sign=919870c5749ebf9d0bb79f380f06d67d",
+"https://focus.youth.cn/article/s?signature=VOZvBzYN5rkDxgX7YwoyGpF85d9oTVdjbgY1L3yAP6WMnmlGK9&uid=50565700&phone_code=eb56c2d93692c0871487e692b21a24d9&scid=36668701&time=1615017306&app_version=1.7.8&sign=9e4c05ae70860a4db8365e5ddc1d030b",
+"https://focus.youth.cn/article/s?signature=3YDwkj8dqQbPnoB4jLk5BDH0bvyJT3A9kkKalgxXL9AJ2zORKM&uid=47692088&phone_code=6b7e436b42d6e438d0bea4c97a61c168&scid=36668458&time=1615017321&app_version=1.7.8&sign=8b091d0192f1ffaa249efbc233e42b8c"]
 
 let encodearticles;
 
