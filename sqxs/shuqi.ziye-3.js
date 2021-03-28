2dd440aa6053ae6c70e89aed41b6af83e49539ce
ip <ip@gmail.com>
2021-03-28 14:27:44 +0800

    Add

--- sqxs/shuqi.ziye-3.js
+++ sqxs/shuqi.ziye-3.js
@@ -122,7 +122,7 @@ GXRZ = '3.19 修复ac报错'
 const $ = Env("书旗小说");
 $.idx = ($.idx = ($.getval('shuqiSuffix') || '1') - 1) > 0 ? ($.idx + 1 + '') : ''; // 账号扩展字符
 const notify = $.isNode() ? require("./sendNotify") : ``;
-const COOKIE = $.isNode() ? require("./shuqiCOOKIE-3") : ``;
+const COOKIE = $.isNode() ? require("./shuqiCOOKIE-2") : ``;
 const logs = 0; // 0为关闭日志，1为开启
 const notifyttt = 1 // 0为关闭外部推送，1为12 23 点外部推送
 const notifyInterval = 2; // 0为关闭通知，1为所有通知，2为12 23 点通知  ， 3为 6 12 18 23 点通知 
