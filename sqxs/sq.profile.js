afcf43b9b326184c3765fddacf4d77c4f84cbadd
ip <ip@gmail.com>
2021-04-08 18:54:02 +0800

    test

--- sqxs/sq.profile.js
+++ sqxs/sq.profile.js
@@ -2222,6 +2222,15 @@ function readlist(timeout = 0) {
                                 await upload() //上传时长
                                 await upload() //上传时长
                                 await upload() //上传时长
+                                await upload() //上传时长
+                                await upload() //上传时长
+                                await upload() //上传时长
+                                await upload() //上传时长
+                                await upload() //上传时长
+                                await upload() //上传时长
+                                await upload() //上传时长
+                                await upload() //上传时长
+                            
                             } else {
                                 console.log(`上传时长：请获取今日时长CK\n`);
                                 $.message += `【上传时长】：请获取今日时长CK\n`;
