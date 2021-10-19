ab53319534ecfaa83e92c4b5207b4fac1537a554
tsuyoshi727 <67669424+tsuyoshi727@users.noreply.github.com>
2021-10-19 18:45:10 +0800

    Update nine.js

--- Task/nine.js
+++ Task/nine.js
@@ -5,7 +5,7 @@ const $ = Env(jobname)
 
 let taskmap = new Map();
 let articleidarr = [];
-let TokenArr = ['oWUxh-Eg_O2-8MGfNjvtlMaGeEV-MRnM', 'tXdVIPdOUd_EmPTN9_yM32-iuURl5-mm','1EbuTWXWGRH_NAz9exfeH5S0mD-sLL-t','gsL4rEetajMKKY0CN5CRZ4ZDbthEY43U','KIQD28DqAAvwaNwQlIigZxi8lJnJcCQU','IsI6GYPInj1is2PUIgZrmalJ3A_TdiHd'];
+let TokenArr = ['oWUxh-Eg_O2-8MGfNjvtlMaGeEV-MRnM', 'tXdVIPdOUd_EmPTN9_yM32-iuURl5-mm','1EbuTWXWGRH_NAz9exfeH5S0mD-sLL-t','zZpNTZC7uU6uMtLKsMFIe30a8pJSUvlf','ihST5L4tuop4uHlsjTFvGzySKY49GEPS','IsI6GYPInj1is2PUIgZrmalJ3A_TdiHd'];
 !(async () => {
     await all();
 })()
